0a70b1025d578268e6b11999f04d5424af256b9d
Rick Lambrechts <rick@lambrechts.nu>
2018-06-11 14:27:39 +0200

    Added basic idee of getting information from client

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ app.get('/', function (req, res) {
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!')
 })
+
+// De data die de server ontvangt is niet encrypted.
+/*
+{ // Headers
+  'hash': 'EclientPrivKey(hash(msg))',
+  'cert': 'EserverPrivKey(eClientPubKey)'
+}
+{ // Body
+  'msg': '123'
+}
+*/
