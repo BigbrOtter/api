26504bf417a6a371176cca7caf01956399a5fb75
kkevindev <34477826+kkevindev@users.noreply.github.com>
2018-06-20 15:53:39 +0200

    api integrity begin
    Co-Authored-By: Jesse van der Ent <jessevdent@users.noreply.github.com>

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,10 +5,12 @@ const router = express.Router()
 // Routes
 const chatRoutes = require('./chat')
 const streamRoutes = require('./stream')
+const integrityRoutes = require('./integrity')
 
 // Use Routes
 router.use('/chat', chatRoutes)
 router.use('/streams', streamRoutes)
+router.use('/integrity', integrityRoutes)
 
 // Export Routes
 module.exports = router
