3f34b51ab9ed458bfd706410b0373255ddef2628
kkevindev <34477826+kkevindev@users.noreply.github.com>
2018-06-21 15:06:08 +0200

    test chat

--- test/test-spec.js
+++ test/test-spec.js
@@ -3,6 +3,15 @@ const chaiHttp = require('chai-http')
 const server = require('../index')
 const should = chai.should()
 const NodeRSA = require('node-rsa')
+const crypto = require('crypto')
+const mongoose = require('mongoose')
+const ObjectId = require('mongoose').Types.ObjectId
+const { User } = require('../models')
+const DBUSER = process.env.DBUSER
+const DBPASS = process.env.DBPASS
+const DBHOST = process.env.DBHOST
+const DBPORT = process.env.DBPORT
+const DBNAME = process.env.DBNAME
 
 chai.use(chaiHttp)
 
@@ -10,36 +19,29 @@ const userCert = process.env.testUserCert
 const userPrivate = process.env.testUserPrivateKey
 
 describe('test chat api', () => {
+  let chat
   it('successful chatbericht', (done) => {
-    chai.request(server).post('/api/chat').set('content-type', 'application/x-www-form-urlencoded').send({
+    const testUserPrivateKey = new NodeRSA(process.env.testUserPrivateKey, 'pkcs1')
+    const message = crypto.randomBytes(20).toString('hex')
+    const body = {
       streamer: 0,
-      message: 'test bericht!',
-      signature: '',
+      message: message,
+      signature: testUserPrivateKey.encryptPrivate(crypto.createHash('sha256').update(message).digest('hex'), 'base64'),
       cert: userCert
-    }).end((err, res) => {
+    }
+    chai.request(server).post('/api/chat').set('content-type', 'application/x-www-form-urlencoded').send(body).end((err, res) => {
+      res.should.have.status(200)
+      res.body.status.should.equal('OK')
+      chat = res.body.chat
       done()
     })
   })
-})
-
-describe('test stream api', () => {
-  it('Succesvol ophalen van alle steams', (done) => {
-    chai.request(server).get('/api/streams').set('cert', userCert).end((err,res) => {
-      if (err) throw err
+  it('successful chats ophalen', (done) => {
+    chai.request(server).get('/api/chat').set('cert', userCert).set('timestamp', chat.timestamp - 1).set('streamer', chat.bsn).end((err, res) => {
       res.should.have.status(200)
-      res.body.should.be.a('array')
+      console.log(res.body)
+      console.log(res.body.chats)
       done()
     })
   })
-  it('Succesvol aanmaken van een stream', (done) => {
-    chai.request(server).post('/api/streams').set('cert', userCert).end((err, res) => {
-      if (err) throw err
-      res.should.have.status(200)
-    })
-  })
-
-})
-
-describe('test integrity api', () => {
-
 })
