9fa9215357deea68347e677e8cc7e9377dfeb64a
kkevindev <34477826+kkevindev@users.noreply.github.com>
2018-06-21 10:47:11 +0200

    voorbeeld test

--- test/test-spec.js
+++ test/test-spec.js
@@ -1,9 +1,25 @@
-var assert = require('assert');
-var calculator = require('../index');
-describe('calculator', function() {
-    describe('add function', function() {
-        it('adds test', function () {
-            assert.equal(true, true);
-        });
-    });
-});
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const server = require('../index')
+const should = chai.should()
+
+describe('test chat api', () => {
+  it('successful chatbericht', (done) => {
+    chai.request(server).post('/api/chat').set('content-type', 'application/x-www-form-urlencoded').send({
+      streamer: 0,
+      message: 'test bericht!',
+      signature: '',
+      cert: ''
+    }).end((err, res) => {
+      done()
+    })
+  })
+})
+
+describe('test stream api', () => {
+
+})
+
+describe('test integrity api', () => {
+
+})
